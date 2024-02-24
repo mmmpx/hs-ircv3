a5e37e2cb8a42050bc03987e93a1fc014ea2e79d
Matteo Paramatti <paramattimatteo@gmail.com>
2024-02-24 14:44:56 +0000

    Export types and update base

--- src/Data/IRCv3.hs
+++ src/Data/IRCv3.hs
@@ -1,6 +1,19 @@
 module Data.IRCv3
-  ( Data.IRCv3.Internal.Parse.message ) where
+  ( message
+  , Message
+  , Tags
+  , Tag
+  , Source
+  , Command
+  , Parameters ) where
 
 import Data.IRCv3.Internal.Parse
   ( message )
 
+import Data.IRCv3.Types
+  ( Message
+  , Tags
+  , Tag
+  , Source
+  , Command
+  , Parameters )
